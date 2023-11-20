0546d46030a0d445adb77537daf9a00d33bea951
Valentin <valentinale203@gmail.com>
2023-11-20 18:46:40 -0300

    Agregue los Nugets y la conexion a la base de datos para hacer la migracion

---
+++ DataAcces/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using API_.Net_Azure.Models;
+
+namespace API_.Net_Azure.DataAcces
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
