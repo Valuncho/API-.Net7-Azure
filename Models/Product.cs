978e1991b6b4458f9849b7dfaacf46c6367b6b85
Valentin <valentinale203@gmail.com>
2023-11-20 18:31:43 -0300

    Creacion de la capa Models

---
+++ Models/Product.cs
@@ -0,0 +1,11 @@
+﻿namespace API_.Net_Azure.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public int stock { get; set; }
+        public DateTime CreateDate { get; set; }
+    }
+}
