acdb67a6cbc02fc9629de3632df7448c5870242c
Valentin <valentinale203@gmail.com>
2023-11-20 19:46:38 -0300

    Creacion de la base de datos exitosa y creacion de la capa DTO y Service

---
+++ DTOs/ProductDto.cs
@@ -0,0 +1,10 @@
+﻿namespace API_.Net_Azure.DTOs
+{
+    public class ProductDto
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public int stock { get; set; }
+        public DateTime CreateDate { get; set; }
+    }
+}
---
+++ Service/ProductService.cs
@@ -0,0 +1,23 @@
+﻿using API_.Net_Azure.DataAcces;
+using Microsoft.Identity.Client;
+
+namespace API_.Net_Azure.Service
+{
+    public class ProductService
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ProductService(ApplicationDbContext context)
+        {
+            this._context = context;
+        }
+
+        public void CreatedProdutct()
+        {
+
+        }
+
+
+
+    }
+}
